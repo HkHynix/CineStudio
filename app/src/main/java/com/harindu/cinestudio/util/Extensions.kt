74f36c4b5ab1ca786d3a9c85804f3b22029b06a1
Harindu <harinduchandrasena@gmail.com>
2025-05-31 18:07:10 +0530

    Succesfully Completed !

--- app/src/main/java/com/harindu/cinestudio/util/Extensions.kt
+++ app/src/main/java/com/harindu/cinestudio/util/Extensions.kt
@@ -30,14 +30,3 @@ fun Fragment.hideKeyboard() {
     view?.hideKeyboard()
 }
 
-fun View.visible() {
-    visibility = View.VISIBLE
-}
-
-fun View.invisible() {
-    visibility = View.INVISIBLE
-}
-
-fun View.gone() {
-    visibility = View.GONE
-}
